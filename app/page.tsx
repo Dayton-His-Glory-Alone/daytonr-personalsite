573537754d0827b1671bcb41d62932f2124bfca2
Dayton Rumbold <45398901+Dayton-His-Glory-Alone@users.noreply.github.com>
2025-03-10 08:27:31 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ const posts = [
   { id: 2, title: "Legacy", content: "When I die, the only thing I want people to say is... I loved Jesus, I loved my wife, and I loved others." },
   { id: 3, title: "Freedom", content: "Freedom never comes without great price." },
   { id: 3, title: "Falling", content: "When you fall: get up, get up, and get up again. Don't let bitterness and regret overtake you." },
+  { id: 4, title: "Worry", content: "We spend so much time worrying about this life, we forget about the next." },
 ];
 
 export default function Home() {
