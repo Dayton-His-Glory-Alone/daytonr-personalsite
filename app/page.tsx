0495463fd7b53cfd1a07ae0f245de79590c12aa0
Dayton Rumbold <45398901+Dayton-His-Glory-Alone@users.noreply.github.com>
2025-09-12 16:19:16 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -310,7 +310,7 @@ export default function Home() {
    <style jsx global>{`
   @keyframes spin-slow {
     from { transform: rotate(0deg); }
-    to { transform: rotate(100deg); }
+    to { transform: rotate(360deg); }
   }
   .animate-spin-slow {
     animation: spin-slow 6s linear infinite alternate;
