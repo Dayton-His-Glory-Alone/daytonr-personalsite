d9b2604d783968da3b30b8f6a471c6134797d46c
Dayton Rumbold <45398901+Dayton-His-Glory-Alone@users.noreply.github.com>
2025-09-12 14:16:28 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react"; 
 
 export default function Home() {
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
